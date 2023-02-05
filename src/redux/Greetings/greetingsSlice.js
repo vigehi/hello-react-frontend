3c414e5bed5619f58a5822940207439d8276e6e1
vigehi <vigehi2017@gmail.com>
2023-02-05 01:15:26 +0100

    Create redux

---
+++ src/redux/Greetings/greetingsSlice.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const baseURL = 'http://localhost:9000/greetings';
+
+const initialState = [];
+
+export const getGreeting = createAsyncThunk(
+  'greetings/getGreeting',
+  async () => {
+    const response = await axios.get(baseURL);
+    return response.data;
+  }
+);
+
+export const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add reducers to handle loading state as needed
+    builder.addCase(getGreeting.fulfilled, (state, action) => action.payload);
+  },
+});
+
+export default greetingsSlice.reducer;
